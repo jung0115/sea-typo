e3cc4d9070d37e3c757503b693315a6ff7fcb7d2
jung0115 <jungim7490@gmail.com>
2024-05-06 11:18:22 +0900

    폰트, 배경화면 적용 + 주소창에 입력한 텍스트로 표시

--- src/App.js
+++ src/App.js
@@ -1,68 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Main from "./Main";
 import './App.css';
 
-// 히스토리의 초기 배열을 생성
-const generateInitialHistory = (length) => {
-  return Array.from({ length }, () => ({ x: 0, y: 0 }));
-};
 
 function App() {
-  const sentence = '마우스를 따라오는 텍스트';
-  const sentenceLength = sentence.length;
-
-  // 히스토리 길이를 문장의 2배 정도로 설정
-  const historyLength = sentenceLength * 7;
-
-  // 마우스 움직임 히스토리 상태를 유지
-  const [history, setHistory] = useState(generateInitialHistory(historyLength));
-
-  useEffect(() => {
-    const handleMouseMove = (e) => {
-      const newPoint = { x: e.clientX, y: e.clientY };
-
-      // 새로운 위치를 추가하고, 히스토리의 길이를 유지
-      setHistory((prevHistory) => {
-        const newHistory = [newPoint, ...prevHistory.slice(0, historyLength - 1)];
-        return newHistory;
-      });
-    };
-
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []); // 빈 배열을 전달하여 한 번만 실행되도록 설정
-
   return (
-    <div className="App">
-      {Array.from({ length: sentenceLength }).map((_, index) => {
-        // 문자 간격을 유지하도록 히스토리의 위치를 계산
-        
-        const historyIndex = index * 7;
-        
-        // 히스토리 인덱스가 범위 내에 있는지 확인
-        if (historyIndex >= history.length) {
-          return null; // 범위를 초과하면 null 반환
-        }
-
-        const position = history[historyIndex]; 
-
-        return (
-          <span
-            key={index}
-            style={{
-              position: 'absolute',
-              left: position.x,
-              top: position.y,
-              transition: 'all 0.2s ease-out',
-            }}
-          >
-            {sentence[index]}
-          </span>
-        );
-      })}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* 동적 경로 */}
+        <Route path="/:text" element={<Main />} />
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Main.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+import './Main.css';
+
+import BackgroundImage from "../src/background.jpeg";
+
+// 히스토리의 초기 배열을 생성
+const generateInitialHistory = (length) => {
+  return Array.from({ length }, () => ({ x: 0, y: 0 }));
+};
+
+function Main() {
+  const { text } = useParams();
+  let sentence = "씨라이프 부산에서 선보이는 바다일렁체";
+  if(text != null && text.length > 0) sentence = text;
+  const sentenceLength = sentence.length;
+
+  // 히스토리 길이를 문장의 2배 정도로 설정
+  const historyLength = sentenceLength * 7;
+
+  // 마우스 움직임 히스토리 상태를 유지
+  const [history, setHistory] = useState(generateInitialHistory(historyLength));
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const newPoint = { x: e.clientX, y: e.clientY };
+
+      // 새로운 위치를 추가하고, 히스토리의 길이를 유지
+      setHistory((prevHistory) => {
+        const newHistory = [newPoint, ...prevHistory.slice(0, historyLength - 1)];
+        return newHistory;
+      });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []); // 빈 배열을 전달하여 한 번만 실행되도록 설정
+
+  return (
+    <Background>
+      {Array.from({ length: sentenceLength }).map((_, index) => {
+        // 문자 간격을 유지하도록 히스토리의 위치를 계산
+        
+        const historyIndex = index * 7;
+        
+        // 히스토리 인덱스가 범위 내에 있는지 확인
+        if (historyIndex >= history.length) {
+          return null; // 범위를 초과하면 null 반환
+        }
+
+        const position = history[historyIndex]; 
+
+        return (
+          <span
+            key={index}
+            style={{
+              position: 'absolute',
+              left: position.x,
+              top: position.y,
+              transition: 'all 0.2s ease-out',
+            }}
+          >
+            {sentence[index]}
+          </span>
+        );
+      })}
+    </Background>
+  );
+}
+
+
+const Background = styled.div`
+  background-image: url(${BackgroundImage});
+  height: 100vh;
+  background-size: auto 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: relative;
+  overflow: hidden;
+  font-family: 'Sea';
+`;
+
+export default Main;
