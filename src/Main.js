7959a667a851d873246a7297397db35a03b23529
jung0115 <jungim7490@gmail.com>
2024-05-16 13:33:53 +0900

    test

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 // import { useParams } from 'react-router-dom';
 
 import './Main.css';
