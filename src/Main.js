4501c1db859dbf0a8cb62f47bd5d47281092e7d3
jung0115 <jungim7490@gmail.com>
2024-05-16 12:47:16 +0900

    폰트, 배경 이미지 파일 변경

--- src/Main.js
+++ src/Main.js
@@ -17,8 +17,6 @@ function Main() {
   // const { text } = useParams();
   // if(text != null && text.length > 0) sentence = text;
 
-  
-
   // 텍스트 입력창의 onChange 이벤트 핸들러
   const handleInputChange = (e) => {
     setSentence(e.target.value); // 입력된 텍스트를 상태로 업데이트
@@ -50,63 +48,86 @@ function Main() {
   }, []); // 빈 배열을 전달하여 한 번만 실행되도록 설정
 
   return (
-    <Background>
-      {/* 텍스트 입력창 */}
-      <Input
-        type="text"
-        value={sentence} // 입력값을 상태로 바인딩
-        onChange={handleInputChange} // 값이 변경될 때 호출될 이벤트 핸들러
-        placeholder="텍스트를 입력하세요"
-      />
+    <Container>
+      <Background>
+        {/* 텍스트 입력창 */}
+        <Input
+          type="text"
+          value={sentence} // 입력값을 상태로 바인딩
+          onChange={handleInputChange} // 값이 변경될 때 호출될 이벤트 핸들러
+          placeholder="텍스트를 입력하세요"
+        />
 
-      {Array.from({ length: sentenceLength }).map((_, index) => {
-        // 문자 간격을 유지하도록 히스토리의 위치를 계산
-        
-        const historyIndex = index * 7;
-        
-        // 히스토리 인덱스가 범위 내에 있는지 확인
-        if (historyIndex >= history.length) {
-          return null; // 범위를 초과하면 null 반환
-        }
+        {Array.from({ length: sentenceLength }).map((_, index) => {
+          // 문자 간격을 유지하도록 히스토리의 위치를 계산
+          
+          const historyIndex = index * 7;
+          
+          // 히스토리 인덱스가 범위 내에 있는지 확인
+          if (historyIndex >= history.length) {
+            return null; // 범위를 초과하면 null 반환
+          }
 
-        const position = history[historyIndex]; 
+          const position = history[historyIndex]; 
 
-        return (
-          <span
-            key={index}
-            style={{
-              position: 'absolute',
-              left: position.x,
-              top: position.y,
-              transition: 'all 0.2s ease-out',
-            }}
-          >
-            {sentence[index]}
-          </span>
-        );
-      })}
-    </Background>
+          return (
+            <span
+              key={index}
+              style={{
+                position: 'absolute',
+                left: position.x,
+                top: position.y,
+                transition: 'all 0.2s ease-out',
+              }}
+            >
+              {sentence[index]}
+            </span>
+          );
+        })}
+      </Background>
+
+      <Download>
+
+      </Download>
+
+    </Container>
   );
 }
 
+const Container = styled.div`
+  width: 100vw;
+`;
 
 const Background = styled.div`
   display: flex;
   justify-content: center;
   align-items: end;
   background-image: url(${BackgroundImage});
-  height: 100vh;
-  background-size: auto 100%;
+  width: 100vw;
+  min-height: 100vh;
+  background-size: 100%;
   background-repeat: no-repeat;
-  background-position: center;
+  background-position: top;
   position: relative;
-  overflow: hidden;
   font-family: 'Sea';
+  overflow-y: auto;
 `;
 
 const Input = styled.input`
   margin-bottom: 40px;
+  padding: 2px 8px;
   width: 28vw;
+  height: 1.3rem;
+  font-family: 'Sea';
+  background-color: transparent;
+  border-width: 1px;
+  border-radius: 3px;
+`;
+
+const Download = styled.div`
+  width: 100vw;
+  background-color: white;
+  height: 10vh;
 `;
 
 export default Main;
